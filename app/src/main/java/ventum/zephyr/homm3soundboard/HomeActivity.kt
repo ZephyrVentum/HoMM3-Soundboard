1536faa28b09e2f48c33e0a009ab558fd3a39af1
Alexander Klimenko <>
2018-08-09 00:44:12 +0300

    init project

---
+++ app/src/main/java/ventum/zephyr/homm3soundboard/HomeActivity.kt
@@ -0,0 +1,12 @@
+package ventum.zephyr.homm3soundboard
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
