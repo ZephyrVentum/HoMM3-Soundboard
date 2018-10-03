3d8cdf71bd9edb1d59574bf0436ee540f508b4ca
Alexander Klimenko <ikytosan0@gmail.com>
2018-10-03 23:14:47 +0300

    some code refactored

--- app/src/main/java/ventum/zephyr/homm3soundboard/HomeActivity.kt
+++ app/src/main/java/ventum/zephyr/homm3soundboard/HomeActivity.kt
@@ -2,7 +2,6 @@ package ventum.zephyr.homm3soundboard
 
 import ventum.zephyr.soundboardtemplate.model.SoundboardCategory
 import ventum.zephyr.soundboardtemplate.ui.SoundboardActivity
-import java.util.*
 
 class HomeActivity : SoundboardActivity() {
     override fun getSoundboardCategories(): ArrayList<SoundboardCategory> {
