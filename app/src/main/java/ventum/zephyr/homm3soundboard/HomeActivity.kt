1e87c52d68ac787addb2178e19bc163bbad99847
Alexander Klimenko <ikytosan0@gmail.com>
2018-10-29 00:36:40 +0200

    added content

--- app/src/main/java/ventum/zephyr/homm3soundboard/HomeActivity.kt
+++ app/src/main/java/ventum/zephyr/homm3soundboard/HomeActivity.kt
@@ -1,5 +1,6 @@
 package ventum.zephyr.homm3soundboard
 
+import android.media.AudioAttributes.*
 import ventum.zephyr.soundboardtemplate.model.SoundItem
 import ventum.zephyr.soundboardtemplate.model.SoundItems
 import ventum.zephyr.soundboardtemplate.model.SoundboardCategory
@@ -8,15 +9,64 @@ import ventum.zephyr.soundboardtemplate.ui.SoundboardActivity
 class HomeActivity : SoundboardActivity() {
     override fun getSoundboardCategories() = ArrayList<SoundboardCategory>().apply {
         add(createSpellsCategory())
+        add(createBattleCategory())
         add(createGameCategory())
     }
 
     private fun createSpellsCategory() = SoundboardCategory(getString(R.string.spell_category), SoundItems().apply {
+        add(SoundItem(R.drawable.antimagic,
+                R.raw.antimagic_sound,
+                R.string.antimagic_spell,
+                soundPool.load(this@HomeActivity, R.raw.antimagic_sound, 1)))
+        add(SoundItem(R.drawable.bloodlust,
+                R.raw.bloodlust_sound,
+                R.string.bloodlust_speel,
+                soundPool.load(this@HomeActivity, R.raw.bloodlust_sound, 1)))
         add(SoundItem(R.drawable.blind,
                 R.raw.blind_sound,
                 R.string.blind_spell,
                 soundPool.load(this@HomeActivity, R.raw.blind_sound, 1)))
     })
+
+   private fun createBattleCategory() = SoundboardCategory(getString(R.string.battle_category), SoundItems().apply {
+       add(SoundItem(R.drawable.bad_morale,
+               R.raw.bad_morale_sound,
+               R.string.bad_morale,
+               soundPool.load(this@HomeActivity, R.raw.bad_morale_sound, 1)))
+       add(SoundItem(R.drawable.battle,
+               R.raw.battle_one,
+               R.string.battle_one,
+               soundPool.load(this@HomeActivity, R.raw.battle_one, 1)))
+       add(SoundItem(R.drawable.battle,
+               R.raw.battle_two,
+               R.string.battle_two,
+               soundPool.load(this@HomeActivity, R.raw.battle_two, 1)))
+       add(SoundItem(R.drawable.battle,
+               R.raw.battle_three,
+               R.string.battle_three,
+               soundPool.load(this@HomeActivity, R.raw.battle_three, 1)))
+       add(SoundItem(R.drawable.battle,
+               R.raw.battle_four,
+               R.string.battle_four,
+               soundPool.load(this@HomeActivity, R.raw.battle_four, 1)))
+       add(SoundItem(R.drawable.battle,
+               R.raw.battle_five,
+               R.string.battle_five,
+               soundPool.load(this@HomeActivity, R.raw.battle_five, 1)))
+       add(SoundItem(R.drawable.battle,
+               R.raw.battle_six,
+               R.string.battle_six,
+               soundPool.load(this@HomeActivity, R.raw.battle_six, 1)))
+       add(SoundItem(R.drawable.battle,
+               R.raw.battle_seven,
+               R.string.battle_seven,
+               soundPool.load(this@HomeActivity, R.raw.battle_seven, 1)))
+       add(SoundItem(R.drawable.battle,
+               R.raw.battle_eight,
+               R.string.battle_eigth,
+               soundPool.load(this@HomeActivity, R.raw.battle_eight, 1)))
+
+   })
 
     private fun createGameCategory() = SoundboardCategory(getString(R.string.game_category), SoundItems().apply {
         add(SoundItem(R.drawable.img_defend,
@@ -32,4 +82,8 @@ class HomeActivity : SoundboardActivity() {
                 R.string.lvl_up,
                 soundPool.load(this@HomeActivity, R.raw.lvl_up, 1)))
     })
+
+    override fun getSoundPoolUsage() = USAGE_UNKNOWN
+
+    override fun getSoundPoolContentType() = CONTENT_TYPE_MOVIE
 }
